32509a190b0bb6b10863e4771232fa65759e8b1b
Zac Garby <zacgarby@icloud.com>
2017-07-10 21:20:14 +0100

    Add two builtin functions for running blocks

--- builtin_fns.py
+++ builtin_fns.py
@@ -1,6 +1,7 @@
 import object as obj
 import ast
-from evaluator import NULL, TRUE, FALSE
+import context
+from evaluator import NULL, TRUE, FALSE, evaluate, err
 
 
 class Builtin(object):
@@ -49,3 +50,36 @@ def input_prompt_builtin(args, context):
         return obj.String(input(args["prompt"]))
     except (KeyboardInterrupt, EOFError):
         return NULL
+        
+@builtin("run $block")
+def run_block_builtin(args, context):
+    block = args["block"]
+    
+    if type(block) != obj.Block:
+        return err("the $block parameter in 'run $block' must be of type <block>")
+    
+    if len(block.params) > 0:
+        return err("since no arguments are provided, $block of 'run $block' must have no parameters")
+    
+    ctx = context.enclose()
+    return evaluate(block.body, ctx)
+    
+@builtin("run $block with $args")
+def run_block_with_args_builtin(args, context):
+    block = args["block"]
+    b_args = args["args"]
+    
+    if type(block) != obj.Block:
+        return err("the $block parameter in 'run $block' must be of type <block>")
+        
+    if type(b_args) != obj.Array:
+        return err("the $args parameter in 'run $block with $args' must be of type <array>")
+    
+    if len(block.params) != len(b_args.elements):
+        return err("the amount of arguments provided in 'run $block with $args' should match the number of parameters in the block")
+    
+    params = [param.value for param in block.params]
+    args_dictionary = dict(zip(params, b_args.elements))
+    
+    ctx = context.enclose_with_args(args_dictionary)
+    return evaluate(block.body, ctx)
