1cc12bc6deef7d1ac5e0b3b6b769e56d73773a36
Zac Garby <zacgarby@icloud.com>
2017-07-12 01:13:41 +0100

    Change version

--- __main__.py
+++ __main__.py
@@ -17,7 +17,7 @@ def main():
     parser.add_argument("-p", "--parse", action="store_true", default=False, help="just parse the file - don't execute it")
     parser.add_argument("-t", "--tree", action="store_true", default=False, help="print the parse tree")
     parser.add_argument("-i", "--interactive", action="store_true", default=False, help="enter interactive mode after the file has been run")
-    parser.add_argument("-v", "--version", action="version", version="pattern-based-language b0.1")
+    parser.add_argument("-v", "--version", action="version", version="pattern-based-language v1.0  --  Copyright Zac Garby © 2017")
     
     args = parser.parse_args()
         
