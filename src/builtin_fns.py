f9c661ad1d91acf06427d4e52137645fdd964d50
Zac Garby <zacgarby@icloud.com>
2017-08-06 15:35:53 +0100

    Do a few more functions

--- src/builtin_fns.py
+++ src/builtin_fns.py
@@ -310,6 +310,13 @@ def pairs_of_obj(args, context):
 
     return obj.Array(pairs)
 
+@arg("n", obj.Number)
+@builtin("round $n")
+def round_n(args, context):
+    n = args["n"]
+    
+    return obj.Number(round(n.value))
+
 @arg("start", obj.Number)
 @arg("end", obj.Number)
 @builtin("$start to $end")
@@ -334,20 +341,6 @@ def start_to_end(args, context):
         return obj.Array([obj.Number(e) for e in range(s_val, e_val)])
     else:
         return start
-
-@arg("num", obj.Number)
-@builtin("square root of $num")
-def square_root_of_num(args, context):
-    return obj.Number(math.sqrt(args["num"].value))
-
-@arg("root", obj.Number)
-@arg("num", obj.Number)
-@builtin("$root st root of $num")
-@builtin("$root nd root of $num")
-@builtin("$root rd root of $num")
-@builtin("$root th root of $num")
-def nth_root_of_num(args, context):
-    return obj.Number(args["num"].value ** (1 / args["root"].value))
 
 @arg("format", obj.String)
 @arg("args", obj.Collection)
